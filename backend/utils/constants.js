1c06805e0dc2be8c5a85a18435153df99e2f9962
Nosareva Victoria <nosareva.vs@yandex.ru>
2023-08-08 20:15:00 +0300

    Внести правки в REG_EXP_LINK

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -6,7 +6,7 @@ const FORBIDDEN_STATUS = 403;
 const NOT_FOUND_ERROR = 404;
 const CONFLICT_ERROR = 409;
 const INTERNAL_SERVER_ERROR = 500;
-const REG_EXP_LINK = /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/;
+const REG_EXP_LINK = /(https?:\/\/)(w{3}\.)?(((\d{1,}\.){3}\d{1,})|((\w-?)+\.(ru|com)))(:\d{2,})?((\/.+)+)?\/?#?/;
 
 module.exports = {
   OK_STATUS,
